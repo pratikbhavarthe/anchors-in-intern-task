972980a4feed4cc4e4c5dbbc86c5a958cae38e97
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2024-01-26 19:27:12 +0530

    Create a account.jsx file

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Emoji from "./components/Emoji";
 import HomePage from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
-import UserDashBoard from "./pages/UserPanel";
+import UserPanel from "./pages/UserPanel";
 
 function App() {
   return (
     <div className="text-white ">
-      <header className="flex sticky top-0 items-center justify-between p-4 bg-black shadow-md z-10">
+      <header className="flex sticky top-0 items-center justify-between p-4 bg-white shadow-md z-10">
         <div className="flex items-center">
           <Emoji symbol="☰" label="menu" className="text-2xl" />
           <span className="px-2">ANONYMOUS</span>
@@ -17,14 +17,14 @@ function App() {
           <Emoji symbol="🧑🏻‍💻" label="user" className="text-2xl" />
         </div>
       </header>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/dashboard" element={<UserDashBoard/>} />
-        </Routes>
-      </Router>
+      <div>
+        <div>
+          <div path="/" element={<HomePage />} />
+          <div path="/login" element={<LoginPage />} />
+          <div path="/signup" element={<SignupPage />} />
+          <div path="/panel" element={<UserPanel/>} />
+        </div>
+      </div>
     </div>
   );
 }
